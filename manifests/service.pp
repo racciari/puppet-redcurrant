3377a5428cf4aea44856a3a5a7232adee9043104
racciari <romain.acciari@gmail.com>
2014-07-31 16:51:58 +0200

    Add dirs to service template

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,9 +2,20 @@ define redcurrant18::service ($type='service',$action='create',$num='1',$options
 
   # Path
   case $options['fhs_compliance'] {
-    'true':  { $sysconfdir = "/etc/redcurrant/${options[ns]}" }
-    'false': { $sysconfdir = "/GRID/${ns}/${options[stgdev]}/conf" }
+    'true': {
+      $rundirname = "/etc/redcurrant/${options[ns]}/run"
+      $sysconfdir = "/etc/redcurrant/${options[ns]}"
+      $localstatedir = "/var/run/redcurrant"
+      $spoolstatedir = "/var/spool/redcurrant/${type}"
+    }
+    'false': {
+      $rundirname = "/GRID/${options[ns]}/${options[stgdev]}/run"
+      $sysconfdir = "/GRID/${options[ns]}/${options[stgdev]}/conf"
+      $localstatedir = "/GRID/${options[ns]}/${options[stgdev]}/run"
+      $spoolstatedir = "/DATA/${hostname}/spool"
+    }
   }
+  if ( )
 
   # File
   case $action {
